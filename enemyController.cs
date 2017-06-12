dfba85912576ece3e8733d5b1a2e78bcb0ef743d
yaeiji <s1240238@gmail.com>
2017-06-12 20:50:14 +0900

    thirdcommit

---
+++ enemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour {
+	public float speed = 1.0f;
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "player") {
+			Destroy (gameObject);
+		
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (-speed, GetComponent<Rigidbody2D> ().velocity.y);	
+	}
+}
